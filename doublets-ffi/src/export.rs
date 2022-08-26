56ae6bcf03eda721f9f82c0fedd3bcad87d7e1b7
uselessgoddess <dodickgod@gmail.com>
2022-08-26 22:02:10 +0300

    Use `Box` instead of raw ptrs

--- doublets-ffi/src/export.rs
+++ doublets-ffi/src/export.rs
@@ -20,22 +20,22 @@ pub unsafe extern "C" fn doublets_create_log_handle(
     max_level: *const c_char,
     use_ansi: bool,
     use_json: bool,
-) -> *mut DoubletsFFILogHandle {
-    assert!(!max_level.is_null());
+) -> Box<DoubletsFFILogHandle> {
+    assert!(
+        !max_level.is_null(), /* `CStr` immediately uses `strlen` */
+    );
     // if str isn't utf-8 just panic
     let max_level_str = CStr::from_ptr(max_level).to_str().unwrap();
-    Box::into_raw(Box::new(DoubletsFFILogHandle::new(
+    Box::new(DoubletsFFILogHandle::new(
         ctx,
         callback,
         max_level_str,
         use_ansi,
         use_json,
-    )))
+    ))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn doublets_free_log_handle(ptr: *mut DoubletsFFILogHandle) {
-    if !ptr.is_null() {
-        let _ = Box::from_raw(ptr);
-    }
+pub unsafe extern "C" fn doublets_free_log_handle(log_handle: Box<DoubletsFFILogHandle>) {
+    let _ = log_handle;
 }
